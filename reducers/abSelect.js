13beca144bbb86b09f23d2895ca9e5f4123cb82d
Eung Jin Lee <oiojin831@gmail.com>
2016-03-17 21:11:38 +0900

    initial commit

---
+++ reducers/abSelect.js
@@ -0,0 +1,10 @@
+const abSelect = (state = {}, action) => {
+  switch (action.type) {
+    case 'SELECT_CARD':
+      return state
+    default:
+      return state
+  }
+}
+
+export default abSelect
---
+++ reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import abSelect from './abSelect'
+
+const menuApp = combineReducers({ abSelect })
+
+export default menuApp
